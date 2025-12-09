65450150c3e6d17cc0f5a59072f003a94fe62509
GJeremy64 <luyckfasseel.jeremy@gmail.com>
2025-12-09 09:46:06 +0100

    Enhance FaqCategory model with fillable and relation
    Added HasFactory trait and defined $fillable for 'name' and 'order'. Implemented faqs() relationship with ordering by 'order' field.

--- app/Models/FaqCategory.php
+++ app/Models/FaqCategory.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FaqCategory extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'order'];
+
+    public function faqs()
+    {
+        return $this->hasMany(Faq::class)->orderBy('order');
+    }
 }
