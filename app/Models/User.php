ff2da4ca9f9374c3734d5b87e7b47720da847c8e
GJeremy64 <luyckfasseel.jeremy@gmail.com>
2025-12-09 09:45:48 +0100

    Extend User model with new fields and relationships
    Added new fillable fields (username, birthday, bio, avatar, is_admin) and updated casts for birthday and is_admin. Implemented relationships to NewsItem, Thread, and Reply models for improved data association.

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,8 +1,9 @@
 <?php
+// filepath: app/Models/User.php
+<?php
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,25 +11,19 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
+        'username',
         'email',
         'password',
+        'birthday',
+        'bio',
+        'avatar',
+        'is_admin',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'two_factor_secret',
@@ -36,17 +31,30 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'birthday' => 'date',
+            'is_admin' => 'boolean',
             'two_factor_confirmed_at' => 'datetime',
         ];
+    }
+
+    // Relationships
+    public function newsItems()
+    {
+        return $this->hasMany(NewsItem::class);
+    }
+
+    public function threads()
+    {
+        return $this->hasMany(Thread::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
     }
 }
