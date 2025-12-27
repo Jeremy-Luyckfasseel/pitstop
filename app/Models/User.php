5a5800d3175ca3bd487fafd892cbf28b7355cf9b
GJeremy64 <luyckfasseel.jeremy@gmail.com>
2025-12-27 23:05:03 +0100

    Update birthday cast format in User model
    Changed the 'birthday' attribute cast to use the 'date:Y-m-d' format for consistency and explicit date formatting.

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,7 +34,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-            'birthday' => 'date',
+            'birthday' => 'date:Y-m-d',
             'is_admin' => 'boolean',
             'two_factor_confirmed_at' => 'datetime',
         ];
