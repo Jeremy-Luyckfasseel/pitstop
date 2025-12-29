70bda6d53ef7d7b46e182469a555229897890914
GJeremy64 <luyckfasseel.jeremy@gmail.com>
2025-12-29 20:29:00 +0100

    Add ContactFormMail Mailable class
    Introduces the ContactFormMail class to handle sending emails from the contact form, passing form data to the email view and setting the subject and reply-to fields dynamically.

---
+++ app/Mail/ContactFormMail.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ContactFormMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(
+        public array $formData
+    ) {
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Contact Form: ' . $this->formData['subject'],
+            replyTo: [$this->formData['email']],
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.contact',
+            with: [
+                'name' => $this->formData['name'],
+                'email' => $this->formData['email'],
+                'subject' => $this->formData['subject'],
+                'messageContent' => $this->formData['message'],
+            ],
+        );
+    }
+}
